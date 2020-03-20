76ab2c0a905b255e675d174d7f91682204da04fc
Candogan Ozdemir <ozdemir.candogan@gmail.com>
2020-03-20 19:55:15 +0300

    -Welcome screen with animation created.

--- app/src/main/java/com/hobbyland/version1/MainActivity.java
+++ app/src/main/java/com/hobbyland/version1/MainActivity.java
@@ -2,13 +2,56 @@ package com.hobbyland.version1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        init();
+        goLoginScreen();
+
+    }
+
+    private void init() {
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        logo = findViewById(R.id.imgv_open_logo);
+        welcomeText = findViewById(R.id.tv_open_welcome);
+
+        logo.setAnimation(topAnim);
+        welcomeText.setAnimation(bottomAnim);
+    }
+
+    private void goLoginScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
