232f9fc38eeb1b5492bbb8868470362dc040c9e4
Candogan Ozdemir <ozdemir.candogan@gmail.com>
2020-05-11 03:58:45 +0300

    -11.05-2020 New activities created. -Card views onclick listeners implemented.

--- app/src/main/java/com/hobbyland/version1/HomeFragment.java
+++ app/src/main/java/com/hobbyland/version1/HomeFragment.java
@@ -1,18 +1,28 @@
 package com.hobbyland.version1;
 
 import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    CardView findPartner;
+    CardView friends;
+    CardView createEvent;
+    CardView joinEvent;
+    CardView lookAround;
+    CardView settings;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,6 +33,75 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        init(view);
+        return view;
+    }
+
+    private void init(View view) {
+        findPartner = view.findViewById(R.id.cv_home_find_partner);
+        friends = view.findViewById(R.id.cv_home_friends);
+        createEvent = view.findViewById(R.id.cv_home_create_event);
+        joinEvent = view.findViewById(R.id.cv_home_join_event);
+        lookAround = view.findViewById(R.id.cv_home_look_around);
+        settings = view.findViewById(R.id.cv_home_settings);
+        //Set on click listener for card views
+        findPartner.setOnClickListener(this);
+        friends.setOnClickListener(this);
+        createEvent.setOnClickListener(this);
+        joinEvent.setOnClickListener(this);
+        lookAround.setOnClickListener(this);
+        settings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cv_home_find_partner:
+                onClickFindPartner();
+                break;
+            case R.id.cv_home_friends:
+                onClickFriends();
+                break;
+            case R.id.cv_home_create_event:
+                onClickCreateEvent();
+                break;
+            case R.id.cv_home_join_event:
+                onClickJoinEvent();
+                break;
+            case R.id.cv_home_look_around:
+                onClickLookAround();
+                break;
+            case R.id.cv_home_settings:
+                onClickSettings();
+                break;
+        }
+    }
+
+    private void onClickSettings() {
+        Toast.makeText(getContext(),"Settings Clicked",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void onClickLookAround() {
+        Toast.makeText(getContext(),"Look Around Clicked",Toast.LENGTH_LONG).show();
+    }
+
+    private void onClickJoinEvent() {
+        Toast.makeText(getContext(),"Join Event Clicked",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void onClickCreateEvent() {
+        Toast.makeText(getContext(),"Create Event Clicked",Toast.LENGTH_LONG).show();
+    }
+
+    private void onClickFriends() {
+        Toast.makeText(getContext(),"Friends Clicked",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void onClickFindPartner() {
+        Toast.makeText(getContext(),"Find Partner Clicked",Toast.LENGTH_LONG).show();
     }
 }
